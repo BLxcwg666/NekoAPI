d5fa5d6b4d2a46c6433c9fb21492e164781321da
libxcnya.so <huixcwg@gmail.com>
2025-01-10 21:42:11 +0800

    fix(ip): unexpected token

--- routes/net/getip.js
+++ routes/net/getip.js
@@ -15,9 +15,9 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
     const userAgent = req.headers["user-agent"];
-    let clientIp = req.headers['x-forwarded-for']?.split(',')[0]?.trim() 
-    || req.headers[process.env.IP_HEADER]?.trim() 
-    || req.ip;
+    let clientIp = req.headers['x-forwarded-for']
+    ? req.headers['x-forwarded-for'].split(',')[0]
+    : req.headers[process.env.IP_HEADER] || req.ip;
 
     if (userAgent && (userAgent.includes("curl") || userAgent.includes("wget"))) {
       res.send(clientIp);
